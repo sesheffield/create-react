945307907b52ba531c4f6668fce1debd4558b00c
Samuel Sheffield <sam@sheffinet.org>
2019-07-14 17:10:41 -0400

    add react component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div id="app">
+            App file
+        </div>
+    )
+}
+
+const root = document.getElementById("root");
+ReactDOM.render(<App/>, root);
